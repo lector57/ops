aacd567aa139dca95f448058363337e9789910a8
lector57 <43914705+lector57@users.noreply.github.com>
2024-03-27 20:44:50 +0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -4,3 +4,4 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-relea
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+##
